bd1a9dc19f5f6b402ada47f640286510dc84c8ad
sdnchapman <sdnchapman@gmail.com>
2018-04-20 08:59:49 +0100

    changed from JAR to WAR

--- src/main/java/com/example/websocketdemo/WebsocketDemoApplication.java
+++ src/main/java/com/example/websocketdemo/WebsocketDemoApplication.java
@@ -2,11 +2,18 @@ package com.example.websocketdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+ 
 @SpringBootApplication
-public class WebsocketDemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(WebsocketDemoApplication.class, args);
-	}
+public class WebsocketDemoApplication extends SpringBootServletInitializer {
+ 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WebsocketDemoApplication.class);
+    }
+ 
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketDemoApplication.class, args);
+    }
 }
--- src/main/java/com/example/websocketdemo/config/WebSocketConfig.java
+++ src/main/java/com/example/websocketdemo/config/WebSocketConfig.java
@@ -1,6 +1,12 @@
 package com.example.websocketdemo.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MessageConverter;
+import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver;
+import org.springframework.messaging.handler.invocation.HandlerMethodReturnValueHandler;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
 
@@ -32,4 +38,40 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                 .setClientPasscode("guest");
         */
     }
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addReturnValueHandlers(List<HandlerMethodReturnValueHandler> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void configureClientInboundChannel(ChannelRegistration arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void configureClientOutboundChannel(ChannelRegistration arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean configureMessageConverters(List<MessageConverter> arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void configureWebSocketTransport(WebSocketTransportRegistration arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 }
